3c82a3d724a50081736767784724eb0e1aa7d095
Julio Rodríguez <julio10489@gmail.com>
2023-01-12 11:28:32 -0500

    se agregaron los keyTypes, lo que permitira hacer los casos de keys

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { CreatePokeContext } from "../../context/CreatePokeContext";
 import { usePokemon } from "../../hooks/usePokemon"
 import { LetterInput } from "../../components/LetterInput/LetterInput";
+import { DELETE_KEY, SPECIAL, LETTERS } from "../../utilities/keyTypes";
 import { Header } from "../../components/header/Header";
 import './game.css';
 
@@ -10,13 +11,24 @@ export function Game(){
   const context = useContext(CreatePokeContext)
   const OPORTUNIDADES = ['primera','segunda','tercerda','cuarta','quinta']; 
   console.log('Context ->> ' + context.pokemon);
+  
   window.addEventListener('keyup', (e)=>{
+    const letra = e.key
     const cells = document.querySelectorAll('.'+OPORTUNIDADES[0])
     const arr = [...cells]
-    const celdaVacia = arr.find((cell, index)=> cell.value == '' || cell.value == undefined)
-    celdaVacia.textContent = e.key
-    celdaVacia.value = e.key
 
+    if(LETTERS.test(letra) && !e.repeat && letra.length <= 1){
+      const celdaVacia = arr.find((cell)=> {
+        if(cell.innerText == '' || cell.innerText  == undefined) return cell
+        else return //componente de notificacion de celdas llenas
+      })
+      celdaVacia.innerText = letra.toUpperCase()
+    }else if(DELETE_KEY.includes(letra)){
+      const celdaLlena = arr.find((cell, index, arrayCell)=> arrayCell[parseInt(index) + 1].innerText == '')
+      celdaLlena.innerText = ''
+    }
+    console.log(letra)
+      
   })
   return(
 
---
+++ src/utilities/keyTypes.js
@@ -0,0 +1,5 @@
+export const DELETE_KEY = ['Backspace', 'Delete']
+
+export const LETTERS = new RegExp('([A-Z])', 'i')
+
+export const SPECIAL = ['Control', 'Tab', 'Shift', 'Alt', 'Enter', 'AltGraph', 'Escapes']
