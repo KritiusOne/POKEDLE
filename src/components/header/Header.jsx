9dea9540f7d7d3d098f5c487858e1fb0aabd2fcb
Julio Rodríguez <julio10489@gmail.com>
2023-04-22 14:25:38 -0500

    mejoras en el layout y comienzo de modificacion de Modal

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
 import { Button } from "../button/Button"
-import { IoIosHelpCircle } from "react-icons/io"
 import { BiDownArrow } from "react-icons/bi"
 import { GiNetworkBars } from "react-icons/gi"
 import { FiSettings } from "react-icons/fi"
 import { useState, useContext } from "react"
 import { CreatePokeContext } from "../../context/CreatePokeContext"
+import {BiHelpCircle} from 'react-icons/bi'
 import "./header.css"
 export function Header() {
   const {
@@ -26,21 +26,21 @@ export function Header() {
   }
   return (
     <header className="Header">
-      <Button handleClick={changeModalAyuda} title="">
-        <IoIosHelpCircle className="header__Button__icon" />
+      <Button handleClick={changeModalAyuda} title="" clase={"Header__button-help"}>
+        <BiHelpCircle className="header__Button__icon" />
       </Button>
       <Button
         handleClick={() => console.log("Boton de titulo")}
-        title="POKEDLE"
+        title="POKEDLE" clase={"Header__button-title"}
       >
         <BiDownArrow className="header__Button__icon" />
       </Button>
       <div className="header__container">
-        <Button handleClick={changeModalRanking} title="">
+        <Button handleClick={changeModalRanking} title="" clase={"Header__button-ranking"}>
           <GiNetworkBars className="header__Button__icon" />
         </Button>
-        <Button handleClick={changeModalConfig} title="">
-          <FiSettings className="header__Button__icon" s />
+        <Button handleClick={changeModalConfig} title="" clase={"Header__button-config"}>
+          <FiSettings className="header__Button__icon"/>
         </Button>
       </div>
     </header>
